e0767fe14d4f018a3bc87cd3a49b32f9148d630e
buzz <buzz@localhost>
2020-02-10 00:42:45 -0500

    First crack at broadcast. Getting terrible server errors, possibly related to accessing a socket twice at the same time...

--- Spades/src/server/ws/WsServer.java
+++ Spades/src/server/ws/WsServer.java
@@ -1,9 +1,15 @@
 package server.ws;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
+import javax.websocket.RemoteEndpoint.Basic;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 /* xxx
@@ -15,8 +21,9 @@ import javax.websocket.server.ServerEndpoint;
 public class WsServer {
 	
 	@OnOpen
-	public void onOpen(){
+	public void onOpen(Session sess){
 		System.out.println("Open Connection ...");
+		SessionManager.manageSession(sess);
 	}
 	
 	@OnClose
@@ -24,10 +31,18 @@ public class WsServer {
 		System.out.println("Close Connection ...");
 	}
 	
+	ArrayList<String> history=new ArrayList();
+	
 	@OnMessage
-	public String onMessage(String message){
+	public String onMessage(String message, boolean last, Session sess){
 		System.out.println("Message from the client: " + message);
 		String echoMsg = "Echo from the Server: " + message;
+		history.add(message);
+		int i=SessionManager.sessionListSize();
+		System.out.println("broadcast(" + i + "):" + message);
+		for (String s:history) {
+			broadcast(s);
+			}
 		return echoMsg;
 	}
 
@@ -36,4 +51,29 @@ public class WsServer {
 		e.printStackTrace();
 	}
 
+	boolean verbose=true;
+	public void broadcast(String msg) {
+		int i;
+		int n=SessionManager.sessionListSize();
+		for (i=0; i<n; i++) {
+			Session sess=SessionManager.getSession(i);
+			Basic br=sess.getBasicRemote();
+			Writer w=null;
+			try {
+				w=br.getSendWriter();
+				} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			try {
+				w.write(msg);
+				} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				}
+			if (verbose) {
+				System.out.println("writing(" + i + "):" + msg);
+			}
+		}
+	}
 }
